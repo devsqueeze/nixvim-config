dd746d534b28ab22db5dba83a4258a73adf65e98
devsqueeze <118290587+devsqueeze@users.noreply.github.com>
2026-08-07 22:15:33 +0100

    Fixed wilder.

--- config/utils/wilder.nix
+++ config/utils/wilder.nix
@@ -1,6 +1,73 @@
+#{ lib, ... }:
+#{
+#  plugins.wilder = {
+#    enable = true;
+#
+#    settings = {
+#      modes = [ "/" "?" ":" ];
+#    };
+#
+#    options = {
+#      use_python_remote_plugin = 0;
+#
+#      pipeline = lib.nixvim.mkRaw ''
+#        {
+#          wilder.branch(
+#            wilder.cmdline_pipeline({
+#              language = 'vim',
+#              fuzzy = 1,
+#              fuzzy_filter = wilder.vim_fuzzy_filter(),
+#            }),
+#            wilder.vim_search_pipeline()
+#          ),
+#        }
+#      '';
+#
+#      renderer = lib.nixvim.mkRaw ''
+#        wilder.popupmenu_renderer(
+#          wilder.popupmenu_border_theme({
+#            highlights = { border = 'Normal' },
+#            border = 'rounded',
+#            pumblend = 20,
+#          })
+#        )
+#      '';
+#    };
+#  };
+#}
+{ lib, ... }:
 {
   plugins.wilder = {
     enable = true;
-    settings.modes = [ "/" "?" ":" ];
+
+    settings = {
+      modes = [ "/" "?" ":" ];
+    };
+
+    options = {
+      use_python_remote_plugin = 0;
+
+      pipeline = lib.nixvim.mkRaw ''
+        {
+          wilder.branch(
+            wilder.cmdline_pipeline({
+              language = 'vim',
+              fuzzy = 1,
+              fuzzy_filter = wilder.vim_fuzzy_filter(),
+            }),
+            wilder.vim_search_pipeline()
+          ),
+        }
+      '';
+
+      renderer = lib.nixvim.mkRaw ''
+        wilder.wildmenu_renderer({
+          highlighter = wilder.basic_highlighter(),
+          separator = ' | ',
+          left = { ' ', wilder.wildmenu_spinner(), ' ' },
+          right = { ' ', wilder.wildmenu_index() },
+        })
+      '';
+    };
   };
 }
